ff4fce28114b10e780b7992ac45809d24c73281d
Mike Kazantsev <mk.fraggod@gmail.com>
2019-12-09 17:50:33 +0500

    +redirector.ml

--- README.rst
+++ README.rst
@@ -5,6 +5,7 @@ waterfox
   :backlinks: none
 
 
+
 Description
 -----------
 
@@ -26,8 +27,9 @@ place as-is, locked-down with mandatory centralized signing.
 .. _Ungoogled Chromium: https://ungoogled-software.github.io/
 
 
-Extensions and tweaks to them
------------------------------
+
+Extensions and tweaks for them
+------------------------------
 
 Any of these can be zipped into xpi like this::
 
@@ -82,3 +84,108 @@ Might be in upstream at some point, see `add-custom-search-engine/pull/13`_.
 
 .. _add-custom-search-engine: https://addons.mozilla.org/en-US/firefox/addon/add-custom-search-engine/
 .. _add-custom-search-engine/pull/13: https://github.com/evilpie/add-custom-search-engine/pull/13
+
+
+
+Misc helpers
+------------
+
+Various useful helper code, data and configuration snippets.
+
+
+redirector.ml
+`````````````
+
+Simple liteweight HTTP stub daemon to serve redirects, translating requests with
+encoded search queries to proper URLs.
+
+Intended to work around Firefox search plugins' limitation of not allowing
+non-encoded search queries, so any URL-building via search keywords is limited
+to GET/POST keywords only.
+
+This workaround is to run simple redirector httpd on localhost, which would
+translate and redirect e.g. ``/github-repo/mk-fg%2Fwaterfox`` from
+"mk-fg/waterfox" query to ``https://github.com/mk-fg/waterfox`` (this repo on github).
+
+Any kind of more complex shortcut-expanding and URL-making logic can be added
+here later as well, limited only by imagination and convenience, potentially
+turning firefox search bar into some kind of command line.
+
+Written in OCaml_ to be relatively fast and liteweight.
+
+Can be compiled with::
+
+  % ocamlopt -o redirector -O2 unix.cmxa str.cmxa redirector.ml
+  % strip redirector
+
+Run with -h/--help for info on command-line options.
+
+.. _OCaml: https://ocaml.org/
+
+
+
+Links to other external stuff
+-----------------------------
+
+- `ff_mozlz4`_ - py3 script to decompress .mozlz4 files like ``search.json.mozlz4``.
+
+- `AppArmor profile`_ - always nice to have for complex apps like browsers.
+
+  More restricted than common distro defaults, with no access to /home outside
+  of xdg junk dirs, profile and ~/Downloads, no access to devices, as well as
+  many other limitations for stuff I don't use myself.
+
+- My `Waterfox-Current Arch Linux PKGBUILD`_
+
+  | Builds it from .tar.gz release archive, not the humongous and slow git checkout.
+  | Can have some local patches/tweaks.
+
+- cgrc_ - systemd-run wrapper for apps that can use some cgroup-limits, like browsers.
+
+  Has a bunch of extra functionality on top of systemd-run and generally usable
+  with just a slice + command name, instead of like 20 common/mandatory options
+  needed with raw systemd-run interface.
+
+.. _ff_mozlz4: https://github.com/mk-fg/fgtk#ff_mozlz4
+.. _AppArmor profile: https://github.com/mk-fg/apparmor-profiles/blob/master/profiles/usr.bin.firefox
+.. _Waterfox-Current Arch Linux PKGBUILD: https://github.com/mk-fg/archlinux-pkgbuilds/tree/master/waterfox-current
+.. _cgrc: https://github.com/mk-fg/fgtk#cgrc
+
+
+
+Links to some outdated stuff
+----------------------------
+
+Mostly scripts and such that I've used with pre-Quantum Firefox,
+which allowed much more customization and had many other diffs in general.
+
+- `ff_backup`_ - browser state tracking script, to auto-commit it into git by cron.
+
+  | For pre-58 firefox only, dumps opened tabs and data for a bunch of old addons.
+  | Generally useful for tab-hoarding and extensions with complex configurations/states.
+
+- `firefox-homepage-generator`_ - old firefox homepage generator.
+
+  Uses profile bookmarks and places dbs plus some other local data to produce
+  custom internet-index page with a bunch of links to browse.
+
+  Python2-based, also for old pre-58 FF versions.
+
+- `convergence`_ - fork of abandoned Convergence addon by Moxie Marlinspike.
+
+  Alternative mechanism for TLS cert validation, which uses distributed "network
+  perspectives" instead of centralized "certificate authorities".
+
+  "Notaries" (perspective-servers) must all agree on same cert signature for
+  IP/host + SNI, which is then cached in local sqlite db (for preformance/privacy)
+  and re-validated only when changed.
+
+  Did maintain working client/server setup of it for a few years and used it myself.
+
+  Was pretty good idea with absolutely terrible commercial CAs back in the day,
+  but less needed now with Certificate Transparency efforts and Let's Encrypt,
+  both mitigating main issues with such centralized model somewhat.
+
+.. _ff_backup: https://github.com/mk-fg/fgtk#ff_backup
+.. _firefox-homepage-generator: https://github.com/mk-fg/firefox-homepage-generator
+.. _convergence: https://github.com/mk-fg/convergence
---
+++ redirector.ml
@@ -0,0 +1,148 @@
+(* Simple liteweight HTTP stub daemon to serve redirects,
+ *   translating requests with encoded search queries to proper URLs.
+ * Intended to run on localhost to be destination for browser search plugins
+ *   to work around their limitations (e.g. encoding in firefox, no complex logic, etc).
+ * Runs a single thread/process and handles all requests sequentially,
+ *   assuming that it's not public internet and no connections will hang or stall intentionally.
+ *
+ * Request format: /<keyword>/<query>
+ * Example translation: /github-repo/mk-fg%2Fwaterfox -> https://github.com/mk-fg/waterfox
+ *
+ * Build with:
+ *   % ocamlopt -o redirector -O2 unix.cmxa str.cmxa redirector.ml
+ *   % strip redirector
+ *)
+
+
+(* Command-line option defaults *)
+let cli_bind = ref "localhost:8888"
+let cli_oneshot = ref false
+let cli_conn_queue = ref 30
+let cli_conn_timeout = ref 3.
+
+(* Mapping of keywords to sites *)
+(* Currently only for simple string-replacement/translation,
+ *   but can be extended with callable logic here as necessary. *)
+let kw_map = Hashtbl.create 4
+let () = (
+	Hashtbl.add kw_map "github-repo" "https://github.com/%q%";
+)
+
+
+(* Command-line args processing *)
+let () = Arg.parse
+	[ ("-b", Arg.Set_string cli_bind, ": host:port to bind listen socket to");
+		("-1", Arg.Set cli_oneshot, ": exit after handling first request") ]
+	(fun arg -> raise (Arg.Bad ("Bogus extra arg : " ^ arg)))
+	("usage: " ^ Sys.argv.(0) ^ " [-b bind] [-1]")
+
+let host, port =
+	let re = Str.regexp "^\\(.*\\):\\([0-9]+\\)$" in
+	if not (Str.string_match re !cli_bind 0)
+		then raise (Arg.Bad ("Incorrect host:port spec : " ^ !cli_bind))
+		else
+			Str.matched_group 1 !cli_bind,
+			int_of_string (Str.matched_group 2 !cli_bind)
+
+(* let () = Printf.printf "opts: bind=[%s:%d] oneshot=%b\n%!" host port !cli_oneshot *)
+
+
+(* Main keyword/query -> http-response mapping logic *)
+(* Currently this only does simple string translation
+ *   mapping with slash-decoding to produce site paths for firefox:
+ * /{kw}/{q} -> {kw-value with %q% replaced} *)
+let kw_res =
+	let res_redirect url = (
+		"HTTP/1.1 302 Found\r\nLocation: "
+		^ url ^ "\r\nContent-Length: 0\r\n\r\n" ) in
+	let res_nx =
+		let pre = "No redirect for: " in
+		( fun url ->
+			let res = pre ^ url in Printf.sprintf
+				"HTTP/1.1 404 Not Found\r\n\
+					Content-Type: text/plain; charset=utf-8\r\n\
+					Content-Length: %d\r\n\r\n%s"
+				(String.length res) res ) in
+	let re_sub, re_sub_tpl = (Str.regexp "%2F", "/") in
+	let kw_map_fmt =
+		let re = Str.regexp "%q%" in
+		(fun site url -> Str.replace_first re url site) in
+	( fun kw q ->
+		let url = Str.global_replace re_sub re_sub_tpl q in
+		try
+			let site = Hashtbl.find kw_map kw in
+			let url = kw_map_fmt site url in
+			res_redirect url
+		with Not_found -> res_nx url )
+
+
+(* Stub HTTP daemon implementation *)
+(* Does not care about or react to any request headers *)
+
+let try_finally f x finally y =
+	let res = try f x with e -> finally y; raise e in finally y; res
+let rec eintr_loop f x =
+	try f x with Unix.Unix_error (Unix.EINTR, _, _) -> eintr_loop f x
+
+let tcp_conn_log conn_ep closed = ()
+(* let tcp_conn_log conn_ep closed =
+ * 	let addr_raw, port =
+ * 		match conn_ep with
+ * 			| Unix.ADDR_INET (a, n) -> (a, n)
+ * 			|  _ -> failwith "not INET" in
+ * 	let addr = Unix.string_of_inet_addr addr_raw in
+ * 	let state = if closed then "-" else "+" in
+ * 	(Printf.printf "conn %s %s:%d\n%!" state addr port) *)
+
+let tcp_server_bind_listen ep queue =
+	let sock = Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
+	try
+		Unix.setsockopt sock Unix.SO_REUSEADDR true;
+		Unix.bind sock ep;
+		Unix.listen sock queue;
+		sock
+	with e -> Unix.close sock; raise e
+
+let tcp_server sock conn_handler =
+	ignore (Sys.signal Sys.sigpipe Sys.Signal_ignore);
+	let rec conn_accept () =
+		let conn, conn_ep = eintr_loop Unix.accept sock in
+		let conn_handle () =
+			tcp_conn_log conn_ep false;
+			conn_handler conn conn_ep in
+		let conn_close () =
+			tcp_conn_log conn_ep true;
+			Unix.close conn in
+		try_finally conn_handle () conn_close ();
+		if !cli_oneshot then () else conn_accept () in
+	conn_accept ()
+
+let conn_handler =
+	let re_req = Str.regexp_case_fold "^GET /\\([^ /]+\\)/\\([^ ]+\\) HTTP" in
+	let buff_len = 2048 in
+	let buff = Bytes.create buff_len in
+	( fun conn conn_ep ->
+		Unix.set_nonblock conn;
+		let select () = Unix.select [conn] [] [conn] !cli_conn_timeout in
+		let rec buff_recv pos =
+			let r, w, x = eintr_loop select () in
+			if List.length x > 0 then 0 else
+				let recv () = Unix.recv conn buff pos (buff_len - pos) [] in
+				let pos = pos + eintr_loop recv () in
+				try Bytes.index buff '\n' with Not_found -> buff_recv pos in
+		ignore (buff_recv 0);
+		let buff_str = Bytes.to_string (Bytes.trim buff) in
+		if not (Str.string_match re_req buff_str 0) then () else
+			let kw = Str.matched_group 1 buff_str in
+			let q = Str.matched_group 2 buff_str in
+			let res = kw_res kw q in
+			let send () = Unix.send_substring conn res 0 (String.length res) [] in
+			Unix.clear_nonblock conn;
+			ignore (eintr_loop send ()) )
+
+let () =
+	let addr = (Unix.gethostbyname host).h_addr_list.(0) in
+	let sock_ep = Unix.ADDR_INET (addr, port) in
+	let sock = tcp_server_bind_listen sock_ep !cli_conn_queue in
+	let server () = tcp_server sock conn_handler in
+	Unix.handle_unix_error server () ;;
