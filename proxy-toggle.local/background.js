099862aa86421a40846f7bcd69fb0db625678526
Mike Kazantsev <mk.fraggod@gmail.com>
2025-06-17 03:48:14 +0500

    proxy-toggle: add "use strict"

--- proxy-toggle.local/background.js
+++ proxy-toggle.local/background.js
@@ -1,10 +1,15 @@
+// Adds button to toggle browser proxy settings between
+//   "System" (typically means "off") and "Manual" (i.e. enabled).
+// Based on Proxy Toggle Button addon. Dunno if private-mode warning is still needed.
+'use strict'
+
 let privateAllowed
 
 let updateIcon = () => browser.proxy.settings.get({}).then(fetched => {
 	let newSetting = fetched.value, enabled = newSetting.proxyType !== 'system'
 	browser.browserAction.setIcon({'path': enabled ? 'proxy-on.svg' : 'proxy-off.svg'})
 	browser.browserAction.setTitle({ 'title':
-		enabled ? 'Proxy ENABLED, click to disable' : 'Proxy DISABLED, click to enable' }) })
+		enabled ? 'Proxy is ENABLED, click to disable' : 'Proxy is DISABLED, click to enable' }) })
 
 let toggleProxy = () => {
 	if (!privateAllowed) return browser.notifications.create(null, {
@@ -13,7 +18,7 @@ let toggleProxy = () => {
 			'In Firefox 67+, Proxy Toggle needs to be allowed in Private Windows.\n' +
 			'Go to Tools > Add-ons > Proxy Toggle, set Run in Private Windows.' })
 	browser.proxy.settings.get({}).then(fetched => {
-		let newSetting = fetched.value;
+		let newSetting = fetched.value
 		newSetting.proxyType = newSetting.proxyType === 'system' ? 'manual' : 'system'
 		browser.proxy.settings.set({'value': newSetting}); updateIcon() }) }
 
